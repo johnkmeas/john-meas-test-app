9fe1be9bd700ccc327491fef4d8065dc0d0cb4f8
johnkmeas <johnkmeas@gmail.com>
2017-03-15 16:38:08 -0700

    implement a delete button for each message, currently not working because of issues with Access-Control-Allow-Headers

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,6 +3,27 @@ import SingleMessage from './SingleMessage.jsx'
 import { BrowserRouter as Router, Link, Route} from 'react-router-dom';
 
 class Message extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  handleDelete(event) {
+
+    event.preventDefault();
+    fetch(`https://john-meas-test.herokuapp.com/messages/${this.props.results.id}`, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Methods': 'DELETE'
+      },
+      method: 'DELETE',
+      mode: 'cors'
+    })
+
+  }
+
   render() {
     console.log("Rendering <Message/>");
     return (
@@ -10,10 +31,13 @@ class Message extends Component {
         <Route exact={true} path="/" render={() => (
           <div>
             <h4> {this.props.results.text}</h4>
-            <p>{this.props.results.created_at}</p>
+            <p>created on: {this.props.results.created_at}</p>
             <Link to={`/g/${this.props.results.id}`}>
-            <h4> {this.props.results.id}</h4>
+              <h5> see more</h5>
             </Link>
+            <form onSubmit={this.handleDelete}>
+              <input type="submit" value="delete" />
+            </form>
             <hr></hr>
           </div>
         )} />
