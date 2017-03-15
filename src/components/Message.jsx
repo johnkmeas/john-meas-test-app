33440eeeec612b08a0b0c77ae34081ed508e2e43
johnkmeas <johnkmeas@gmail.com>
2017-03-15 13:54:09 -0700

    implemented Router to link to a Single message view

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,19 +1,33 @@
 import React, {Component} from 'react';
-
+import { BrowserRouter as Router, Link, Route} from 'react-router-dom';
 class Message extends Component {
   render() {
 
     console.log("Rendering <Message/>");
     return (
       <div>
-        <h4> {this.props.results.id}</h4>
-        <h4> {this.props.results.text}</h4>
-        <p>{this.props.results.created_at}</p>
-        <hr></hr>
+        <Route exact={true} path="/" render={() => (
+          <div>
+            <h4> {this.props.results.text}</h4>
+            <p>{this.props.results.created_at}</p>
+            <Link to={`/g/${this.props.results.id}`}>
+            <h4> {this.props.results.id}</h4>
+            </Link>
+            <hr></hr>
+          </div>
+        )} />
+
+        <Route path="/g/:msgid" component={SingleMessage}/>
       </div>
     );
   }
 }
 
+
+const SingleMessage = ({ match}) => (
+  <div>
+    {match.params.msgid}
+  </div>
+)
 export default Message;
 
