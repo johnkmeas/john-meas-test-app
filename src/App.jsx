58215dd6f76662c13f49ec76a9227a271aeb1436
johnkmeas <johnkmeas@gmail.com>
2017-03-17 18:52:58 -0700

    moved router and routes into App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
-import MessageList from './components/MessageList'
-
+import MessageList from './components/MessageList';
+import SingleMessage from './components/SingleMessage.jsx';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <MessageList />
+        <Router>
+        <div>
+          <Route exact={true} path="/" render={() => (
+            <MessageList />
+
+            )} />
+            <Route path="/messages/:id" component={SingleMessage}/>
+            </div>
+          </Router>
       </div>
     );
   }
