69adf483dd49e638add443414ac0c82a8c13fc19
johnkmeas <johnkmeas@gmail.com>
2017-03-17 18:59:13 -0700

    singleMessage component now successfully renders a single message

--- src/components/SingleMessage.jsx
+++ src/components/SingleMessage.jsx
@@ -1,30 +1,41 @@
 import React, {Component} from 'react';
-import Message from './Message.jsx';
+import Moment from 'react-moment';
+
 class SingleMessage extends React.Component {
   constructor() {
     super();
-    this.state = {}
+    this.state = {item: []}
   }
   componentWillMount() {
-    fetch(`https://john-meas-test.herokuapp.com/messages/${this.props.match.params.id}`)
-    .then( (response) => {
-      let myData = response.json()
-      this.setState({myData})
+    fetch(`https://john-meas-test.herokuapp.com/messages/${this.props.match.params.id}/`)
+    .then(function(response) {
+       return response.json();
+    })
+    .then( (item) => {
+      this.setState({item: this.state.item.concat(item)})
     })
   }
 
   render() {
-    console.log('this.state', this.state)
-  let items = this.state;
+    console.log('this.state', this.state.item)
+    const items = this.state.item;
 
     return (
         <div>
-          <p>need to implement single message display </p>
+          {items.map((item,i) =>
+            <div key={i}>
+            <p>{item.author}</p>
+             <p> {item.text}</p>
+            </div>
+          )}
+
+
         </div>
 
     )
   }
 }
+
 
 
 
