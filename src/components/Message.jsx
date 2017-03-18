53fa766e879dc32c0bf7dfd64e6b513cceea43f9
johnkmeas <johnkmeas@gmail.com>
2017-03-18 01:32:59 -0700

    refactoring and added className

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,27 +1,19 @@
 import React, {Component} from 'react';
 import Moment from 'react-moment';
-import SingleMessage from './SingleMessage.jsx'
-import { BrowserRouter as Router, Link, Route} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class Message extends Component {
   constructor(props) {
     super(props);
-
     this.handleDelete = this.handleDelete.bind(this);
   }
 
   handleDelete(event) {
 
     fetch(`https://john-meas-test.herokuapp.com/messages/${this.props.results.id}/`, {
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      method: 'DELETE',
-      mode: 'cors'
+      method: 'DELETE'
     })
     console.log('event',event )
-    event.preventDefault();
 
   }
 
@@ -29,13 +21,14 @@ class Message extends Component {
     console.log("Rendering <Message/>");
     return (
       <div>
-        <h4> {this.props.results.text}</h4>
-        <p>created on: <Moment>{this.props.results.created_at}</Moment></p>
-        <Link to={`/messages/${this.props.results.id}`}>
-          <h5> see more</h5>
-        </Link>
-        <button onClick={this.handleDelete}>Delete</button>
-        <hr></hr>
+        <div className="message">
+          <h4> {this.props.results.text}</h4>
+          <p>Posted <Moment fromNow>{this.props.results.created_at}</Moment></p>
+          <Link to={`/messages/${this.props.results.id}`}>
+            <h5> see more</h5>
+          </Link>
+          <button onClick={this.handleDelete}>Delete</button>
+        </div>
       </div>
     );
   }
